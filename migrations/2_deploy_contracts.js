7deea582e52f728e15524bcfe21e9d48256fd30f
N F <something@something.com>
2018-10-16 19:48:49 +0200

    migration setup

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require('./ConvertLib.sol')
-var MetaCoin = artifacts.require('./MetaCoin.sol')
+var PiggyBank = artifacts.require('./PiggyBank.sol')
 
-module.exports = function (deployer) {
-  deployer.deploy(ConvertLib)
-  deployer.link(ConvertLib, MetaCoin)
-  deployer.deploy(MetaCoin)
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(PiggyBank, "Schoolbank", accounts[0], {value: 1000000});
 }
